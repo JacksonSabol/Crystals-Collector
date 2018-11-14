b788d99657e8b74075206a0b6b5b3f37c518e6c7
Jackson Sabol <JacksonSabol@users.noreply.github.com>
2018-11-13 17:30:12 -0800

    add 4 crystal button pictures to assets; add border to game container; define image array in JS and define initial variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+// IDs (#) to reference for pushing to HTML elements
+// game-over
+// number-to-guess
+// user-total
+// wins
+// losses
+// crystals
+
+// Create array of crystal button images
+// Numbered out of order because I want the images with dual crystals on the outside edges
+var crystalImages = ["assets/images/crystalButton2.png", "assets/images/crystalButton1.png", "assets/images/crystalButton3.png", "assets/images/crystalButton4.png"];
+
+// Set initial variables
+var wins = 0;
+var losses = 0;
+var numberToGuess = 0;
+var userTotal = 0;
