dfb2493c161c10f71316e8df2195d88b5854e6a0
Jackson Sabol <JacksonSabol@users.noreply.github.com>
2018-11-13 21:47:24 -0800

    Add a unique id to the 2 right-most images so I can float them right in the stylesheet and make a space in the middle; float those images right in stylesheet; change random number generators to floor method to try and make them produce duplicate values less frequently

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,6 @@
-// IDs (#) to reference for pushing to HTML elements
-// Spans:
-// generate-crystals - if needed, game could just restart after win or lose by calling beginGame
-// game-over
-// number-to-guess
-// user-total
-// wins
-// losses
-// Div:
-// crystals
-
 // Create array of crystal button images
 // Numbered out of order because I want the images with dual crystals on the outside edges
-var crystalImages = ["assets/images/crystalButton2.png", "assets/images/crystalButton1.png", "assets/images/crystalButton3.png", "assets/images/crystalButton4.png"];
+var crystalImages = ["assets/images/crystalButton2.png", "assets/images/crystalButton1.png", "assets/images/crystalButton4.png", "assets/images/crystalButton3.png"];
 
 // Set initial variables
 var wins = 0;
@@ -25,13 +14,13 @@ beginGame();
 // Define function to begin the game
 function beginGame() {
     // Generate the random number to guess - per the Homework Instructions: 'The random number shown at the start of the game should be between 19 - 120.'
-    numberToGuess = Math.round(Math.random() * 101) + 19;
+    numberToGuess = Math.floor(Math.random() * 102) + 19;
     // Display the random number on the page in the #number-to-guess span
     $("#number-to-guess").text(numberToGuess);
     // Reset the player's total even after a game has been played through
     userTotal = 0;
     // Display the player's total on the page in the #total-score span for each iteration of clickCrystal and beginGame
-    $("#total-score").text(userTotal);
+    $("#user-total").text(userTotal);
 
     // Iterate through the length of the crystal images array to generate img tags for each crystal, add source attributes, add classes, and assign random number values
     for (i = 0; i < crystalImages.length; i++) {
@@ -42,15 +31,17 @@ function beginGame() {
         // Add src attribute to each img tag equal to the 'URL' we assigned in the crystalImages array
         crystalBtn.attr("src", crystalImages[i]);
         // Add 'data-' attribute to each img tag equal to a random number so we can reference that value when a player clicks on a crystal - per the Homework Instructions: 'Each crystal should have a random hidden value between 1 - 12.'
-        crystalBtn.attr("data-crystalvalue", Math.round(Math.random() * 11) + 1);
+        crystalBtn.attr("data-crystalvalue", Math.floor(Math.random() * 12) + 1);
+
+        // Add a unique id to the 2 right-most images so I can float them right in the stylesheet and make a space in the middle
+        // i > 1 and i < crystalImages.length (4) limits the images that receive the id to the last 2 in the array - the right-most two images
+        if (i > 1 && i < crystalImages.length) {
+            // Attribute 
+            crystalBtn.attr("id", "img" + i);
+        }
         // Append all of the img tags we just made and their attributes/classes to the div with ID #crystals on the HTML
         $("#crystals").append(crystalBtn);
     };
-    // Testing the randomly generated data value for each crystal
-    // $(".crystal-image").on("click", function () {
-    //     var crystalValue = ($(this).attr("data-crystalvalue"));
-    //     console.log(crystalValue);
-    // });
 
     // Call clickCrystal function by clicking on any img with the class of .crystal-image to begin assessment of crystal values
     $(".crystal-image").on("click", clickCrystal);
@@ -60,8 +51,6 @@ function beginGame() {
     //
     // Figure out a way to ensure that at least one of the crystals always has a value of 1, just as a win 'fail-safe'
     //
-    // Figure out how to move the right two crystal images to the right side of the container so it looks better
-    // maybe this: crystalBtn.attr("id", "i"); then position ids 2 and 3 to the right in the CSS
 };
 
 // Define function to collect data from user clicks on crystals for comparison to randomly generated number to guess
